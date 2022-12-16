f7b8e97de0f9a8ed6ba1f01b06e169bb6ddf0c61
FantinPro <raimbault.fantin.pro@gmail.com>
2022-12-16 14:10:55 +0100

    feat: add Field component

---
+++ src/components/Field.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  component: {
+    type: String,
+    required: true
+  },
+  modelValue: {
+    type: [String, Number, Boolean],
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <component
+    :is="component"
+    :type="props.type !== 'select' ? props.type : undefined"
+    :name="props.name"
+    :value="modelValue"
+    :checked="modelValue"
+    @input="$emit('update:modelValue', type == 'checkbox' ? $event.target.checked : $event.target.value)"
+  >
+    <slot />
+  </component>
+</template>
